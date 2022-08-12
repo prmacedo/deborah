f3e79da1914198764550449444264b312734e766
prmacedo <p.ricardomacedo@hotmail.com>
2022-08-12 10:40:34 -0300

    create treatment page

---
+++ treatment.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Deborah</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <!-- Hover.css -->
+  <link rel="stylesheet" href="./css/hover-min.css">
+
+  <!-- Fontes -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Vidaloka&display=swap" rel="stylesheet">
+
+  <!-- Meu CSS -->
+  <link rel="stylesheet" href="./css/global.css">
+  <link rel="stylesheet" href="./css/treatment.css">
+</head>
+<body>
+  <?php require_once './partials/navbar.php' ?>
+
+  <main>
+    <div id="banner-1"></div>
+
+    <div class="content container px-3">
+      <h1 class="font-vidaloka fsz-3 text-center mb-3">
+        TRATAMENTO 1
+      </h1>
+
+      <p class="text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna molestie at elementum eu. Non sodales neque sodales ut. Non arcu risus quis varius. Vitae tortor condimentum lacinia quis vel eros. In nisl nisi scelerisque eu ultrices vitae. Sed id semper risus in hendrerit gravida. Elit eget gravida cum sociis natoque penatibus et. Tellus pellentesque eu tincidunt tortor aliquam nulla. Enim ut sem viverra aliquet eget sit. Tincidunt nunc pulvinar sapien et ligula ullamcorper.
+      </p>
+
+      <p class="text-justify">
+        Purus faucibus ornare suspendisse sed nisi lacus. Vitae turpis massa sed elementum tempus egestas. Morbi enim nunc faucibus a pellentesque sit amet. Proin libero nunc consequat interdum varius. Senectus et netus et malesuada. Eu scelerisque felis imperdiet proin fermentum leo vel. Lacus vel facilisis volutpat est velit. Sit amet justo donec enim diam vulputate. Eu consequat ac felis donec et. Aenean pharetra magna ac placerat. Velit sed ullamcorper morbi tincidunt ornare massa. Parturient montes nascetur ridiculus mus mauris vitae. Sit amet porttitor eget dolor morbi non arcu. Nulla facilisi morbi tempus iaculis. Neque sodales ut etiam sit amet nisl purus. Tincidunt id aliquet risus feugiat in ante metus dictum.
+      </p>
+
+      <div class="text-center mt-5">
+        <a href="#" id="my-btn" class="btn">Agendar horário</a>
+      </div>
+    </div>
+  </main>
+
+
+  <?php require_once './partials/footer.php' ?>
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="./js/scripts.js"></script>
+</body>
+</html>
