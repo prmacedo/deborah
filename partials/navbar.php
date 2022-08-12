d0dfd0c450259b74cd50839bdaf39cf18032a10c
prmacedo <p.ricardomacedo@hotmail.com>
2022-08-12 10:44:42 -0300

    add navigation

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,30 +1,30 @@
 <nav class="navbar navbar-expand-lg fixed-top">
   <div class="container d-flex justify-content-between">
-    <a class="navbar-brand" href="#">Logo</a>
+    <a class="navbar-brand" href="./index.php#">Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item hvr-underline-from-center">
-          <a class="nav-link active" aria-current="page" href="#">Início</a>
+          <a class="nav-link active" aria-current="page" href="./index.php#">Início</a>
         </li>
         <li class="nav-item hvr-underline-from-center">
-          <a class="nav-link" href="#">Clínica</a>
+          <a class="nav-link" href="./index.php#welcome">Clínica</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="./index.php#treatment" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Tratamentos
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Tratamento 1</a></li>
-            <li><a class="dropdown-item" href="#">Tratamento 2</a></li>
-            <li><a class="dropdown-item" href="#">Tratamento 3</a></li>
-            <li><a class="dropdown-item" href="#">Tratamento 4</a></li>
+            <li><a class="dropdown-item" href="treatment.php">Tratamento 1</a></li>
+            <li><a class="dropdown-item" href="treatment.php">Tratamento 2</a></li>
+            <li><a class="dropdown-item" href="treatment.php">Tratamento 3</a></li>
+            <li><a class="dropdown-item" href="treatment.php">Tratamento 4</a></li>
           </ul>
         </li>
         <li class="nav-item hvr-underline-from-center">
-          <a class="nav-link" href="#">Blog</a>
+          <a class="nav-link" href="./index.php#blog">Blog</a>
         </li>
         <li class="nav-item">
           <a class="nav-link p-0 me-1" href="#">
@@ -35,7 +35,7 @@
           </a>
         </li>     
         <li id="contact-btn" class="nav-item border-25">
-          <a class="nav-link" href="#contact">Fale conosco</a>
+          <a class="nav-link" href="./index.php#contact">Fale conosco</a>
         </li>        
       </ul>
     </div>
